aea85d24802738e3e2579264b6f47662d7c450a4
Nathan Schill <nateschill@gmail.com>
2025-07-04 00:07:19 -0600

    added pre-commit for formatting and linting

---
+++ tool/setup_git_hooks.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+Future<void> main() async {
+  final preCommitHook = File('.git/hooks/pre-commit');
+  await preCommitHook.parent.create();
+  await preCommitHook.writeAsString('''
+#!/bin/sh
+exec flutter pub run dart_pre_commit
+''');
+
+  if (!Platform.isWindows) {
+    final result = await Process.run('chmod', ['a+x', preCommitHook.path]);
+    stdout.write(result.stdout);
+    stderr.write(result.stderr);
+    exitCode = result.exitCode;
+  }
+}
