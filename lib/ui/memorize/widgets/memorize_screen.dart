d35d5b9b2bf0551dadc41d354df0546424bf8180
Nathan Schill <nateschill@gmail.com>
2025-07-07 21:24:55 -0600

    got memorize screen mostly working

--- lib/ui/memorize/widgets/memorize_screen.dart
+++ lib/ui/memorize/widgets/memorize_screen.dart
@@ -12,12 +12,81 @@ class MemorizeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('Memorize')),
       body: SafeArea(
-        child: Center(
+        child: Container(
+          padding: EdgeInsets.fromLTRB(8, 8, 8, 8),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [Text(viewModel.memo.name), Text(viewModel.memo.content)],
+            spacing: 8,
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  viewModel.name,
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+              ),
+              ListenableBuilder(
+                listenable: Listenable.merge([
+                  viewModel.fractionWordsKeepListenable,
+                  viewModel.keepFirstLettersListenable,
+                ]),
+                builder: (_, _) {
+                  return Expanded(
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: SingleChildScrollView(
+                        child: Text(
+                          viewModel.content,
+                          style: Theme.of(context).textTheme.bodyLarge,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+              SliderRow(viewModel: viewModel),
+            ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class SliderRow extends StatefulWidget {
+  const SliderRow({super.key, required this.viewModel});
+
+  final MemorizeViewModel viewModel;
+
+  @override
+  State<SliderRow> createState() => _SliderRowState();
+}
+
+class _SliderRowState extends State<SliderRow> {
+  @override
+  Widget build(final BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Row(
+        children: [
+          Switch(
+            value: widget.viewModel.keepFirstLetters,
+            onChanged: (final bool value) {
+              setState(() {
+                widget.viewModel.keepFirstLetters = value;
+              });
+            },
+          ),
+          Expanded(
+            child: Slider(
+              value: widget.viewModel.fractionWordsKeep,
+              onChanged: (final double value) {
+                setState(() {
+                  widget.viewModel.fractionWordsKeep = value;
+                });
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
