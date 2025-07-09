7a55e7ad73e3fda8022654d85b36492ab90f8b72
Nathan Schill <nateschill@gmail.com>
2025-07-08 23:37:06 -0600

    injected database and drafted MemoService

--- lib/config/dependencies.dart
+++ lib/config/dependencies.dart
@@ -1,8 +1,16 @@
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
+import 'package:sqflite/sqflite.dart';
 
 import '../data/repositories/memo_repository.dart';
+import '../data/services/memo_service.dart';
 
-List<SingleChildWidget> get providersLocal {
-  return [Provider(create: (final context) => MemoRepository())];
+List<SingleChildWidget> providersLocal({required final Database db}) {
+  return [
+    Provider<Database>.value(value: db),
+    Provider(create: (final context) => MemoService(db: context.read())),
+    Provider(
+      create: (final context) => MemoRepository(memoService: context.read()),
+    ),
+  ];
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:path/path.dart' as p;
 import 'package:provider/provider.dart';
+import 'package:sqflite/sqflite.dart';
 
 import 'config/dependencies.dart';
 import 'routing/router.dart';
 
-void main() {
-  runApp(MultiProvider(providers: providersLocal, child: const MainApp()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final Database db = await initializeDatabase();
+
+  runApp(
+    MultiProvider(
+      providers: providersLocal(db: db),
+      child: const MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
@@ -17,7 +27,33 @@ class MainApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
-      routerConfig: router,
+      routerConfig: router(context.read()),
     );
   }
 }
+
+Future<Database> initializeDatabase() async {
+  final String databasesPath = await getDatabasesPath();
+  final String dbPath = p.join(databasesPath, 'app.db');
+
+  // I could turn this into a command in MemoService and await it.
+  return await openDatabase(
+    dbPath,
+    version: 1,
+    onCreate: (final Database db, final int version) async {
+      await db.execute('''
+        CREATE TABLE IF NOT EXISTS Memo (
+          id INTEGER PRIMARY KEY AUTOINCREMENT, 
+          name TEXT NOT NULL, 
+          content TEXT NOT NULL, 
+          keepFirstLetters INT, 
+          fractionWordsKeep REAL
+        )
+      ''');
+    },
+  );
+}
+
+// Future<Database> initializeDatabase() async {
+//   return await MemoService.open();
+// }
